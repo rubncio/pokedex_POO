886c49b6a21b46b3ff39c8934d040bbbe9f3e491
RubenCano <erreceeme20@gmail.com>
2026-01-30 11:28:29 +0100

    definicion test de movimientos con tipos

--- prueba/tests/test_pokemon.py
+++ prueba/tests/test_pokemon.py
@@ -1,10 +1,23 @@
 import pytest
 from pokemon import Pokemon
+from movimiento import Movimiento
 from exceptions.oponenteVacio import OponenteVacio
 pokemon=Pokemon(nombre="pikachu", tipo="fuego", nivel=4, vida=150, fuerza=14, defensa=10, velocidad=58, movimientos=[Movimiento("fuego",33),Movimiento("agua",95),Movimiento("naturaleza",41),Movimiento("magia",50)]
     )
+
 def ejecutarMovimientoOponenteVacio():
     with pytest.raises(OponenteVacio):
         pokemon.ejecutar_movimiento()
 
-def recibirDaño():
+def setMovimientoIncompatible():
+    with pytest.raises(TypeError):
+        movimientoAgua=Movimiento("agua",95)
+        pokemon.setMovimiento(movimientoAgua)
+
+def setMovimientoCompatible():
+    with pytest.raises(TypeError):
+        movimientoFuego=Movimiento("fuego",95)
+        pokemon.setMovimiento(movimientoFuego)
+
+
+    
